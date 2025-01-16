1d8d5e0c7bd7e0296ad8f78b6d7f12ee8b6bfcf6
AwirutJr <luckych315@gmail.com>
2025-01-17 01:40:06 +0700

    update

--- src/app/components/Contect/page.jsx
+++ src/app/components/Contect/page.jsx
@@ -1,32 +1,40 @@
 import Link from 'next/link'
+import { FaFacebook } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 
 
 const page = () => {
   return (
-    <div id="contect" className=" w-full h-[350px]">
-      <div className="border w-[300px]">
-        <h1 className="font-bold mb-3">Contact Information</h1>
-        <p>+6695-807-2692</p>
-        <p>91/835 Nuanjan 56 Alley, Section 1, Nuanjan Subdistrict, Bueng Kum District, Bangkok 10230, Thailand</p>
+    <div id="contect" className=" w-full flex h-[300px] iii">
+            <img src="/photo/chok.jpg" className='w-[300px] h-full object-cover'/>
+      <div className=' w-full h-[300px] flex justify-center  items-start gap-8 py-16'>
+          <div className=" w-[300px]">
+            <h1 className="font-bold mb-3">Contact Information</h1>
+            <p>+6695-807-2692</p>
+            <p>91/835 Nuanjan 56 Alley, Section 1, Nuanjan Subdistrict, Bueng Kum District, Bangkok 10230, Thailand</p>
+          </div>
+          <div className=' w-[300px] flex flex-col items-center gap-1.5'>
+          <h1 className="font-bold mb-3">Important Links</h1>
+            <Link href='#'>Home</Link>
+            <Link href='#'>About Me</Link>
+            <Link href='#'>Skill</Link>
+            <Link href='#'>Project</Link>
+            <Link href='#'>Contect</Link>
+          </div>
+          <div className="w-[300px] flex flex-col items-center">
+            <h1 className="font-bold mb-3">Social Media Links</h1>
+            <ul className='flex gap-2'>
+              <Link href='#'><FaFacebook className='w-8 h-8'/></Link>
+              <Link href='#'><MdEmail className='w-8 h-8'/></Link>
+              <Link href='#'><FaGithub className='w-8 h-8'/></Link>
+              <Link href='#'><FaLinkedin className='w-8 h-8'/></Link>
+              <Link href='#'><FaInstagram className='w-8 h-8'/></Link>
+            </ul>
+          </div>
       </div>
-      <div className='border w-[300px]'>
-      <h1 className="font-bold mb-3">Social Media Links</h1>
-        <Link href='#'>Home</Link>
-        <Link href='#'>About Me</Link>
-        <Link href='#'>Skill</Link>
-        <Link href='#'>Project</Link>
-        <Link href='#'>Contect</Link>
-      </div>
-      <div className="border w-[500px]">
-        <h1 className="font-bold mb-3">Important Links</h1>
-        <Link href='#'>facebook: Awirut jiensakul</Link><br />
-        <Link href='#'>Email: luckych315@gmail.com</Link>
-        <Link href='#'>Github: AwirutJr</Link>
-        <Link href='#'>Linkin: Awirut Jiensakul</Link>
-        <Link href='#'>IG: awirut_ck</Link>
-      </div>
-      <div><p>© 2025 AwirutJr. All rights reserved.</p></div>
-
     </div>
   )
 }
